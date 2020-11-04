413fb0b1bdcf155fd19d397265f69947149b5a70
tek-f <aaron.falco2@gmail.com>
2020-11-04 20:58:04 +1100

    Update

---
+++ Assets/Scripts/Mirror/NetworkSpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GunBall.Mirror
+{
+    public class NetworkSpawnPoint : MonoBehaviour
+    {
+        private void Awake()
+        {
+            NetworkSpawnSystem.AddSpawnPoint(transform);
+        }
+        private void OnDestroy()
+        {
+            NetworkSpawnSystem.RemoveSpawnPoint(transform);
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position, 1f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 2);
+        }
+    }
+}
---
+++ Assets/Scripts/Mirror/NetworkSpawnSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System.Linq;
+
+namespace GunBall.Mirror
+{
+    public class NetworkSpawnSystem : NetworkBehaviour
+    {
+        [SerializeField] private GameObject playerPrefab = null;
+
+        private static List<Transform> spawnPoints = new List<Transform>();
+
+        private int nextIndex = 0;
+
+        public static void AddSpawnPoint(Transform transform)
+        {
+            spawnPoints.Add(transform);
+
+            spawnPoints.OrderBy(x => x.GetSiblingIndex()).ToList();
+        }
+
+        public static void RemoveSpawnPoint(Transform transform) => spawnPoints.Remove(transform);
+
+        public override void OnStartServer() => NetworkLobbyManager.OnServerReadied += SpawnPlayer;
+
+        [ServerCallback]
+        private void OnDestroy() => NetworkLobbyManager.OnServerReadied -= SpawnPlayer;
+        [Server]
+        public void SpawnPlayer(NetworkConnection conn)
+        {
+            Transform spawnPoint = spawnPoints.ElementAtOrDefault(nextIndex);
+
+            if (spawnPoint == null)
+            {
+                Debug.LogError("Spawn point transform not found");
+                return;
+            }
+
+            GameObject playerInstance = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+            NetworkServer.Spawn(playerInstance, conn);
+            nextIndex++;
+            if (nextIndex >= spawnPoints.Count)
+            {
+                nextIndex = 0;
+            }
+        }
+    }
+}
